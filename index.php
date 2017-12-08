f4dce3ce1c8a6f8b2fb0bb400fdf71046e1c3cbe
Felix <felixbartolomeh@gmail.com>
2017-12-08 05:55:04 +0100

    Iniciamos

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Curso JS</title>
+	<link rel="stylesheet" type="text/css" href="css/micss.css">
+	<script src="eventos.js"></script>
+	<script src="Jquery/jquery-3.2.1.min.js"></script>
+	<script>
+	$(function () {
+		
+		$("body").hide(). fadeIn(3000);
+			
+		});
+	</script>
+	
+</head>
+<body>
+	<h1>Curso JS</h1>
+	<img id="imagen" src="images/arbol.jpg" width="35%">
+</body>
+</html>
