66710874a24ba2872259ab951c6577adf9314e22
Felix <felixbartolomeh@gmail.com>
2017-12-09 01:56:09 +0100

    Cambios de centrado de margenes y color texto del articulo principal

--- index.php
+++ index.php
@@ -20,13 +20,13 @@
 	<h1>Curso JS</h1>
 	<p>Creando página de pruebas </p>
 	<img id="imagen" src="images/arbol.jpg" width="35%">
-	<article>
-		Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+	<article class="articulo-principal">
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
 		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
 		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
 		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 	</article>
 </div>
 </body>
